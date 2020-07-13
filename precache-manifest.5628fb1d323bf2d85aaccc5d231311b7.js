fd14b9f4365f3c121ac57eb63954f2c3f9d822fc
choidy180 <choidy180@naver.com>
2020-07-13 23:01:56 +0900

    Updates

---
+++ precache-manifest.5628fb1d323bf2d85aaccc5d231311b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0d62fbca019a341986d990b4dae97f2",
+    "url": "/movie-app2/index.html"
+  },
+  {
+    "revision": "22bef821b69c8a5b4eb3",
+    "url": "/movie-app2/static/css/main.ae4cfe19.chunk.css"
+  },
+  {
+    "revision": "18887685efcba530b72d",
+    "url": "/movie-app2/static/js/2.481f46c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app2/static/js/2.481f46c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22bef821b69c8a5b4eb3",
+    "url": "/movie-app2/static/js/main.4051bd43.chunk.js"
+  },
+  {
+    "revision": "6dac7286ce5d9750d0a7",
+    "url": "/movie-app2/static/js/runtime-main.e3a223a7.js"
+  }
+]);
