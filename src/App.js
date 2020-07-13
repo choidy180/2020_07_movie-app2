7994ffa807d4e98b8d7c0a941f04625cd659a33d
choidy180 <52563196+choidy180@users.noreply.github.com>
2020-07-13 19:40:17 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,61 +1,39 @@
 import React from 'react';
-import PropTypes from "prop-types";
 
-function Drama({ name, poster, rating }) {
-  return (
-    <div>
-      <img src={poster} alt={name}/>
-      <h1>{name}</h1>
-      <h4>{rating}/5</h4>
-    </div>
-  )
-}
-
-Drama.prototype = {
-  name: PropTypes.string,
-  poster: PropTypes.string,
-  rating: PropTypes.number
-}
-
-const Drama_Tvn = [
-  {
-    id: 1,
-    name: "나의 아저씨",
-    poster: "http://cfile296.uf.daum.net/image/999C573F5AFDC02E07B887",
-    rating: 5
-  },
-  {
-    id: 2,
-    name: "비밀의 숲",
-    poster: "http://cfile249.uf.daum.net/image/25EF5C33597EEFE432FB4A",
-    rating: 4.7
-  },
-  {
-    id: 3,
-    name: "도깨비",
-    poster: "http://cfile251.uf.daum.net/image/2732BF4F589876DD266788",
-  },
-  {
-    id: 4,
-    name: "인간수업",
-    poster: "http://t1.daumcdn.net/cafeattach/mEr9/abaffb775038e54e9b7aabc0a2357110d57530d4",
-    rating: "4.9"
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    console.log("hello");
   }
-]
-
-function App() {
-  return (
-    <div>
-      {Drama_Tvn.map(content => (
-        <Drama
-          key={content.id}
-          name={content.name}
-          poster={content.poster}
-          rating={content.rating}
-        />
-      ))}
-    </div>
-  );
+  state = {
+    count: 0
+  };
+  plus = () => {
+    console.log("plus")
+    this.setState(current => ({count: current.count + 1}));
+  };
+  minus = () => {
+    console.log("minus")
+    this.setState({count: this.state.count - 1});
+  };
+  // 컴포넌트 렌더링 할때
+  componentDidMount(){
+    console.log("component render");
+  }
+  // 업데이트시 
+  componentDidUpdate(){
+    console.log("I just update");
+  }
+  render() {
+    console.log("render");
+    return (
+      <div>
+        <h1>I number is {this.state.count}</h1>
+        <button onClick={this.plus}>PLUS</button>
+        <button onClick={this.minus}>MINUS</button>
+      </div>
+    )
+  }
 }
 
 export default App;
